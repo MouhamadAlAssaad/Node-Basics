6f4bbcb4a40d5081676c85ba3bdf7513217b8772
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-23 23:19:20 +0200

    configurable save file

--- tasks.js
+++ tasks.js
@@ -1,72 +1,60 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
 
-
+const fs = require("fs");
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n' || text === 'exit\n') {
+  if (text === "quit\n" || text === "exit\n") {
     quit();
-  }
-  else if(text === 'hello\n' || text.split(" ")[0] === "hello" ){
+  } else if (text === "hello\n" || text.split(" ")[0] === "hello") {
     hello(text);
-  }
-  else if(text === 'list\n'){
+  } else if (text === "list\n") {
     list();
-  }
-  else if(text === 'help\n'){
+  } else if (text === "help\n") {
     help();
-  }
-  else if (text.startsWith('add')){
+  } else if (text.startsWith("add")) {
     add(text);
-  }
-  else if (text.startsWith('remove')){
+  } else if (text.startsWith("remove")) {
     removeTask(text);
-  } 
-  else if (text.startsWith('edit')){
+  } else if (text.startsWith("edit")) {
     edit(text);
-  }
-  else if (text.startsWith('check')){
+  } else if (text.startsWith("check")) {
     check(text);
-  }
-  else if (text.startsWith('uncheck')){
+  } else if (text.startsWith("uncheck")) {
     uncheck(text);
-  }
-
-  else{
+  } else {
     unknownCommand(text);
   }
 }
-
 
 /**
  * prints "unknown command"
@@ -75,53 +63,47 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(text){
-  if (text === "hello\n"){
+function hello(text) {
+  if (text === "hello\n") {
     console.log("hello!");
-    return
+    return;
   }
-  text = text.replace('\n', '').trim();
+  text = text.replace("\n", "").trim();
   const typed = text.split(" ");
-  if (typed[0] === "hello"){
+  if (typed[0] === "hello") {
     const txt = typed.slice(1).join(" ");
     console.log(`hello ${txt}!`);
-    }
+  }
 }
 
-
-
-
-
-function add(obj){
-  obj=obj.trim().split(" ")[1]
-  if (obj==undefined){
-    console.log("please insert an object")
-  }
-  else{
-    tasks.push(`[ ] ${obj}`)
+function add(obj) {
+  obj = obj.trim().split(" ")[1];
+  if (obj == undefined) {
+    console.log("please insert an object");
+  } else {
+    tasks.push(`[ ] ${obj}`);
   }
 }
 
 function removeTask(obj) {
-  obj = obj.replace('\n', '').trim();
+  obj = obj.replace("\n", "").trim();
   if (obj === "remove\n") {
     tasks.pop();
-    return
+    return;
   }
-  
-  const words = obj.split(' ');
-  if (words[0] === 'remove') {
-    const b = words.slice(1).join(' ');
+
+  const words = obj.split(" ");
+  if (words[0] === "remove") {
+    const b = words.slice(1).join(" ");
     if (b > tasks.length) {
       console.log("You enter a number does not exist");
     } else {
@@ -130,45 +112,40 @@ function removeTask(obj) {
   }
 }
 
-
-
-
-function edit(text){
-  if (text === "edit\n"){
-
+function edit(text) {
+  if (text === "edit\n") {
     console.log("error!");
-    return
+    return;
   }
-  text=  text.replace('\n',' ').trim();
-  const objects = text.split(' ');
-  if(objects [0] === 'edit'){
-    const b= objects.slice(1).join(' ');
-    if (b[0] > tasks.length){
-      console.log("Doesn't exist")
-    }
-    else if (typeof Number(b[0]) === "number"  && b[1] === " ") {
+  text = text.replace("\n", " ").trim();
+  const objects = text.split(" ");
+  if (objects[0] === "edit") {
+    const b = objects.slice(1).join(" ");
+    if (b[0] > tasks.length) {
+      console.log("Doesn't exist");
+    } else if (typeof Number(b[0]) === "number" && b[1] === " ") {
       tasks.splice(`${b[0] - 1}`, 1, b.slice(2));
-  }  else if (typeof b[0] === "string") {
-    tasks.splice(-1, 1, b)
-  }
+    } else if (typeof b[0] === "string") {
+      tasks.splice(-1, 1, b);
+    }
   }
 }
 
 function check(text) {
   if (text === "check\n") {
-    console.log("error!")
-    return
+    console.log("error!");
+    return;
   }
-  text = text.replace('\n', '').trim()
-  const objects = text.split(" ")
+  text = text.replace("\n", "").trim();
+  const objects = text.split(" ");
   if (objects[0] === "check") {
-    const b = objects.slice(1).join(' ')
+    const b = objects.slice(1).join(" ");
     if (b[0] > tasks.length) {
-      console.log("Doesn't exist")
-    } else if (tasks[b[0]-1][1] === "✓") {
+      console.log("Doesn't exist");
+    } else if (tasks[b[0] - 1][1] === "✓") {
       console.log("This is already checked!");
     } else {
-      tasks.splice(`${b[0] - 1}`, 1, `[✓]${tasks[b - 1].slice(3)}`)
+      tasks.splice(`${b[0] - 1}`, 1, `[✓]${tasks[b - 1].slice(3)}`);
     }
   }
 }
@@ -181,72 +158,39 @@ function check(text) {
 
 function uncheck(text) {
   if (text === "uncheck\n") {
-    console.log("Error!")
-    return
+    console.log("Error!");
+    return;
   }
-  text = text.replace('\n', '').trim();
-  const objects = text.split(' ');
+  text = text.replace("\n", "").trim();
+  const objects = text.split(" ");
   if (objects[0] === "uncheck") {
-    const b = objects.slice(1).join(' ')
+    const b = objects.slice(1).join(" ");
     if (b[0] > tasks.length) {
-      console.log("Doesn't exist")
-    } else if (tasks[b[0]-1][1] === " ") {
+      console.log("Doesn't exist");
+    } else if (tasks[b[0] - 1][1] === " ") {
       console.log("Thiss is already unchecked!");
-    }  else {
-      tasks.splice(`${b[0] - 1}`, 1, `[ ]${tasks[b - 1].slice(3)}`)
+    } else {
+      tasks.splice(`${b[0] - 1}`, 1, `[ ]${tasks[b - 1].slice(3)}`);
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /**
  * The help command gives you the commands used to help you in your code
  *
  * @returns {void}
  */
-function help(){
-  let helpp='hello for saying hello \n quit or exit to quit the application \n help to get the commands \n add to add new elements \n  remove and but the number of the items to remove items '
-  console.log(helpp)
+function help() {
+  let helpp =
+    "hello for saying hello \n quit or exit to quit the application \n help to get the commands \n add to add new elements \n  remove and but the number of the items to remove items ";
+  console.log(helpp);
 }
 
-let tasks = ["task" , "task1"]
-function list(){
-  tasks.forEach((objects , i) => {
-    console.log(`${i +1} - ${objects}`)
-  
-  })
-
+let tasks = ["task", "task1"];
+function list() {
+  tasks.forEach((objects, i) => {
+    console.log(`${i + 1} - ${objects}`);
+  });
 }
 /**
  * Exits the application
@@ -258,32 +202,25 @@ function list(){
 //   process.exit();
 // }
 
-
 try {
-  let data = fs.readFileSync(savefile);
+  let data = fs.readFileSync(sfile);
   var listOb = JSON.parse(data);
-}
-catch (e) {
-  console.log(`Hello from JSON!`)
+} catch (e) {
+  console.log(`Hello from JSON!`);
 }
 if (listOb !== undefined) {
   tasks = listObject.list;
 } else {
-  listOb = { "list": [] }
+  listOb = { list: [] };
   tasks = listOb.list;
 }
-
-
-
-
-
 
 function quit() {
   let fs = require("fs");
   let data = JSON.stringify(objList);
   try {
     fs.writeFileSync("./database.json", data);
-    console.log(`Saving changes...`)
+    console.log(`Saving changes...`);
   } catch (error) {
     console.error(error);
   }
@@ -295,6 +232,26 @@ function quit() {
 
 
 
+let sfile;
+if (process.argv[2] == null) {
+  sfile = "database.json";
+} else {
+  sfile = process.argv[2];
+}
+
+try {
+  let data = fs.readFileSync(sfile);
+  var listOb = JSON.parse(data);
+}
+catch (nono) {
+  console.log(`Hello from JSON!`)
+}
+if (listOb !== undefined) {
+  list = listOb.list;
+} else {
+  listOb = { "list": [] }
+  list = listOb.list;
+}
 
 
 
@@ -305,12 +262,4 @@ function quit() {
 
 
 // The following line starts the application
-startApp("Mouhamad Al Assaad")
-
-
-
-
-
-
-
-
+startApp("Mouhamad Al Assaad");
